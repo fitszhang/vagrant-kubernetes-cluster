861b44e0d4a88b59b8546d489cc4bfadd80b4483
fitszhang <fits.zhang@gmail.com>
2022-03-10 17:39:36 +0800

    Update bootstrap.sh

--- ubuntu/bootstrap.sh
+++ ubuntu/bootstrap.sh
@@ -10,11 +10,12 @@ echo "[TASK 0] Setting TimeZone"
 timedatectl set-timezone Asia/Shanghai
 
 echo "[TASK 1] Setting DNS"
-sed -i -e 's/#DNS=/DNS=8.8.8.8/' /etc/systemd/resolved.conf
+cat >/etc/systemd/resolved.conf <<EOF
+[Resolve]
+DNS=8.8.8.8
+EOF
 systemctl daemon-reload
 systemctl restart systemd-resolved.service
-mv /etc/resolv.conf /etc/resolv.conf.bak
-ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf
 
 echo "[TASK 2] Setting Ubuntu System Mirrors"
 cat >/etc/apt/sources.list<<EOF
@@ -33,7 +34,7 @@ deb-src http://mirrors.aliyun.com/ubuntu/ focal-proposed main restricted univers
 deb http://mirrors.aliyun.com/ubuntu/ focal-backports main restricted universe multiverse
 deb-src http://mirrors.aliyun.com/ubuntu/ focal-backports main restricted universe multiverse
 EOF
-apt update -qq >/dev/null 2>&1
+apt update -qq  # >/dev/null 2>&1
 
 echo "[TASK 3] Disable and turn off SWAP"
 sed -i '/swap/d' /etc/fstab
@@ -59,21 +60,21 @@ EOF
 sysctl --system >/dev/null 2>&1
 
 echo "[TASK 7] Install containerd runtime"
-apt install -qq -y containerd apt-transport-https >/dev/null 2>&1
+apt install -y containerd apt-transport-https
 mkdir /etc/containerd
 containerd config default > /etc/containerd/config.toml
 # 配置containerd镜像源
-# 替换k8s.gcr.io为registry.aliyuncs.com/k8sxio
+# 替换k8s.gcr.io为registry.aliyuncs.com/google_containers
 # 替换https://registry-1.docker.io为https://registry.cn-hangzhou.aliyuncs.com
 # 设置docker.io的镜像地址为https://bqr1dr1n.mirror.aliyuncs.com
-# 设置k8s.gcr.io的镜像地址为https://registry.aliyuncs.com/k8sxio
-sed -i "s#k8s.gcr.io#registry.aliyuncs.com/k8sxio#g"  /etc/containerd/config.toml
+# 设置k8s.gcr.io的镜像地址为https://registry.aliyuncs.com/google_containers
+sed -i "s#k8s.gcr.io#registry.aliyuncs.com/google_containers#g"  /etc/containerd/config.toml
 sed -i 's#SystemdCgroup = false#SystemdCgroup = true#g' /etc/containerd/config.toml
 sed -i "s#https://registry-1.docker.io#https://registry.cn-hangzhou.aliyuncs.com#g"  /etc/containerd/config.toml
 sed -i '/\[plugins\.\"io\.containerd\.grpc\.v1\.cri\"\.registry\.mirrors\]/ a\\ \ \ \ \ \ \ \ [plugins."io.containerd.grpc.v1.cri".registry.mirrors."docker.io"]\n\ \ \ \ \ \ \ \ \ \ endpoint = ["https://bqr1dr1n.mirror.aliyuncs.com"]' /etc/containerd/config.toml
-sed -i '/\[plugins\.\"io\.containerd\.grpc\.v1\.cri\"\.registry\.mirrors\]/ a\\ \ \ \ \ \ \ \ [plugins."io.containerd.grpc.v1.cri".registry.mirrors."k8s.gcr.io"]\n\ \ \ \ \ \ \ \ \ \ endpoint = ["https://registry.aliyuncs.com/k8sxio"]' /etc/containerd/config.toml
+sed -i '/\[plugins\.\"io\.containerd\.grpc\.v1\.cri\"\.registry\.mirrors\]/ a\\ \ \ \ \ \ \ \ [plugins."io.containerd.grpc.v1.cri".registry.mirrors."k8s.gcr.io"]\n\ \ \ \ \ \ \ \ \ \ endpoint = ["https://registry.aliyuncs.com/google_containers"]' /etc/containerd/config.toml
 systemctl daemon-reload
-systemctl enable containerd --now >/dev/null 2>&1
+systemctl enable containerd --now
 systemctl restart containerd
 
 echo "[TASK 8] Add apt repo for kubernetes"
@@ -81,10 +82,10 @@ curl -s https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg | apt-key add 
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main
 EOF
-apt update -qq >/dev/null 2>&1
+apt update
 
 echo "[TASK 9] Install Kubernetes components (kubeadm, kubelet and kubectl)"
-apt install -qq -y kubeadm=1.22.0-00 kubelet=1.22.0-00 kubectl=1.22.0-00 >/dev/null 2>&1
+apt install -y kubeadm=1.22.0-00 kubelet=1.22.0-00 kubectl=1.22.0-00
 crictl config runtime-endpoint /run/containerd/containerd.sock
 crictl config image-endpoint /run/containerd/containerd.sock
 
@@ -94,7 +95,7 @@ echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
 systemctl reload sshd
 
 echo "[TASK 11] Set root password"
-echo -e "kubeadmin\nkubeadmin" | passwd root >/dev/null 2>&1
+echo -e "kubeadmin\nkubeadmin" | passwd root
 echo "export TERM=xterm" >> /etc/bash.bashrc
 
 echo "[TASK 12] Update /etc/hosts file"
